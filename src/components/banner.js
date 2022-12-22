92dbc7d58dd141893457476262c6a3957aa62ea3
Jorrit van der Plaats <jvdplaats@hotmail.com>
2022-12-22 19:09:49 +0100

    first commit

---
+++ src/components/banner.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+// import { StaticImage } from "gatsby-plugin-image";
+import { container } from "./banner.module.css";
+
+const Banner = () => {
+  return (
+    <div className={container}>
+      <h1>Silbo Herreño</h1>
+      <h3>Una tradición más viva que nunca</h3>
+      {/* <StaticImage src="src/images/example.png" /> */}
+    </div>
+  );
+};
+
+export default Banner;
