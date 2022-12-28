e6e53138e2503f08ad1d7a1f13f8219693956c7c
Jorrit van der Plaats <jvdplaats@hotmail.com>
2022-12-28 15:00:29 +0100

    add images to

--- src/components/casas.js
+++ src/components/casas.js
@@ -2,6 +2,27 @@ import * as React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import { casasContainer } from "./casas.module.scss";
 
+const width = "150px";
+
+const CasaImagenes = [
+  <StaticImage src="../images/069-Poblado-xxxx-1024x768.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1309-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1310-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1312-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1315-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1327-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1332-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1334-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1337-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1338-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1339-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1347-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1348-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1356-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1360-300x225.jpg" width={width} />,
+  <StaticImage src="../images/IMG_1367-300x225.jpg" width={width} />,
+];
+
 const Casas = () => {
   return (
     <div className={casasContainer}>
